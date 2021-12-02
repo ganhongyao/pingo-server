3cea3d06c77188b4cd246811d01e1d89663fb422
Hong Yao <ganhongyao@u.nus.edu>
2021-12-02 23:03:43 +0800

    Set up sockets

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 const express = require("express");
+const socketIo = require("socket.io");
+const http = require("http");
+
 const app = express();
-const server = require("http").createServer(app);
-const io = require("socket.io").listen(server);
-const port = 80;
+const server = http.createServer(app);
+
+const port = process.env.PORT || 4001;
+
+const index = require("./routes/index");
+
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(index);
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
