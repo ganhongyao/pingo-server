378f53f6b07e2c4bf3f99399a3b317f09b10bc30
Hong Yao <ganhongyao@u.nus.edu>
2021-12-03 18:08:35 +0800

    Store locations of clients

--- app.js
+++ app.js
@@ -18,16 +18,28 @@ const io = socketIo(server, {
 
 app.use(index);
 
+const locations = new Map();
+
 io.on("connection", (socket) => {
   console.log("New client connected");
+  locations.set(socket.id, {});
+
   socket.broadcast.emit("friend connection", "Hello from server");
+
   socket.on("location update", (location) => {
-    console.log("location update");
-    console.log(socket);
-    console.log(location);
+    console.log("Location update");
+    locations.set(socket.id, location);
+    socket.broadcast.emit("friend location update", location);
   });
+
+  socket.on("query friend locations", () => {
+    socket.emit("friend locations", Object.fromEntries(locations));
+    console.log("Query friend locations");
+  });
+
   socket.on("disconnect", () => {
     console.log("Client disconnected");
+    locations.delete(socket.id);
   });
 });
 
